c5a0d30691d9a3f4c5db9b74aa72f05fe4510bfd
Gustavo Brendler <gmbrendler@inf.ufpel.edu.br>
2022-02-04 15:51:45 -0300

    add next/image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
@@ -8,11 +9,21 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://i.ibb.co/Lg7Xd8J/perfil-de-avatar-de-homem-no-icone-redondo-24640-14044.webp" alt="Imagem de Brendler17" />
+      <Image
+        src="https://i.ibb.co/Lg7Xd8J/perfil-de-avatar-de-homem-no-icone-redondo-24640-14044.webp"
+        alt="Imagem de Brendler17"
+        width={100}
+        height={100}
+      />
       <div>
         <strong>Gustavo Brendler</strong>
         <p>
-          <img src="icons/level.svg" alt="" />
+          <Image 
+            src="icons/level.svg" 
+            alt="level_icon" 
+            width={100}
+            height={100}
+          />
           Level {level}
         </p>
       </div>
